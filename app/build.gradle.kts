e8916ccef98f7f9a43cd6e8b184bb31440848622
TatsukiIshijima <tatsuki445@gmail.com>
2023-07-16 11:08:26 +0900

    add hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,8 +2,10 @@ import java.io.FileInputStream
 import java.util.Properties
 
 plugins {
+  kotlin("kapt")
   id("com.android.application")
   id("org.jetbrains.kotlin.android")
+  id("com.google.dagger.hilt.android")
 }
 
 val keystorePropertiesFile = rootProject.file("local.properties")
@@ -84,6 +86,8 @@ dependencies {
   implementation("androidx.compose.ui:ui-tooling-preview")
   implementation("androidx.compose.material3:material3")
   implementation("com.amazon.device:amazon-appstore-sdk:3.0.4")
+  implementation("com.google.dagger:hilt-android:2.44")
+  kapt("com.google.dagger:hilt-android-compiler:2.44")
   testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -91,4 +95,8 @@ dependencies {
   androidTestImplementation("androidx.compose.ui:ui-test-junit4")
   debugImplementation("androidx.compose.ui:ui-tooling")
   debugImplementation("androidx.compose.ui:ui-test-manifest")
+}
+
+kapt {
+  correctErrorTypes = true
 }
