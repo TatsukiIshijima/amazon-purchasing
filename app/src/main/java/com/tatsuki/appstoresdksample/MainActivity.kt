ec57ef4a50c698fb2a4b3297ff984db87a05da2e
TatsukiIshijima <tatsuki445@gmail.com>
2023-07-16 11:34:25 +0900

    setup hilt

--- app/src/main/java/com/tatsuki/appstoresdksample/MainActivity.kt
+++ app/src/main/java/com/tatsuki/appstoresdksample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.tatsuki.appstoresdksample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,10 +13,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.tatsuki.appstoresdksample.ui.theme.AppStoreSDKSampleTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+  private val mainViewModel: MainViewModel by viewModels()
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
     setContent {
       AppStoreSDKSampleTheme {
         // A surface container using the 'background' color from the theme
