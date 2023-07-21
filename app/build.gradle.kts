0d8f4bc8326f13223d16e5c146eb2447c69675b7
TatsukiIshijima <tatsuki445@gmail.com>
2023-07-21 22:29:29 +0900

    up version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
     applicationId = "com.tatsuki.appstoresdksample"
     minSdk = 21
     targetSdk = 33
-    versionCode = 2
+    versionCode = 3
     versionName = "1.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
