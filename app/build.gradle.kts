084f4d2d4039f7da3aac6c9fb080aba34246a979
TatsukiIshijima <tatsuki445@gmail.com>
2023-07-23 21:58:13 +0900

    down version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
     applicationId = "com.tatsuki.amazonpurchasingsample"
     minSdk = 21
     targetSdk = 33
-    versionCode = 5
+    versionCode = 1
     versionName = "1.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
