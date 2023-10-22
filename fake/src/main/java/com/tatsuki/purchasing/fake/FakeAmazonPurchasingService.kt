7027d291198f070885e04b21cf798d2a9dc88889
TatsukiIshijima <tatsuki445@gmail.com>
2023-10-22 11:59:38 +0900

    impl fake

--- fake/src/main/java/com/tatsuki/purchasing/fake/FakeAmazonPurchasingService.kt
+++ fake/src/main/java/com/tatsuki/purchasing/fake/FakeAmazonPurchasingService.kt
@@ -4,17 +4,26 @@ import android.content.Context
 import com.amazon.device.iap.PurchasingListener
 import com.amazon.device.iap.internal.model.ProductBuilder
 import com.amazon.device.iap.internal.model.ProductDataResponseBuilder
+import com.amazon.device.iap.internal.model.PurchaseResponseBuilder
+import com.amazon.device.iap.internal.model.PurchaseUpdatesResponseBuilder
 import com.amazon.device.iap.internal.model.UserDataBuilder
 import com.amazon.device.iap.internal.model.UserDataResponseBuilder
 import com.amazon.device.iap.model.FulfillmentResult
 import com.amazon.device.iap.model.Product
 import com.amazon.device.iap.model.ProductDataResponse
 import com.amazon.device.iap.model.ProductType
+import com.amazon.device.iap.model.PurchaseResponse
+import com.amazon.device.iap.model.PurchaseUpdatesResponse
 import com.amazon.device.iap.model.RequestId
 import com.amazon.device.iap.model.UserDataResponse
 import com.tatsuki.purchasing.core.AmazonPurchasingService
+import com.tatsuki.purchasing.fake.db.FakeAmazonReceiptDb
+import com.tatsuki.purchasing.fake.model.FakeAmazonReceiptData
+import com.tatsuki.purchasing.fake.model.FakeAmazonUser
 
-class FakeAmazonPurchasingService : AmazonPurchasingService {
+class FakeAmazonPurchasingService(
+  private val fakeAmazonReceiptDb: FakeAmazonReceiptDb
+) : AmazonPurchasingService {
 
   private var status: FakeServiceStatus = FakeServiceStatus.Available
 
@@ -24,8 +33,10 @@ class FakeAmazonPurchasingService : AmazonPurchasingService {
 
   private lateinit var purchasingListener: PurchasingListener
   private var isEnablePendingPurchases = false
+  private var purchaseUpdatesPageNumber = 1
+  private val purchaseUpdatesPageSize = 3
 
-  // Amazon user account
+  // Current amazon user account
   private var amazonUser = FakeAmazonUser()
 
   // Sku list of subscription products defined in the Amazon developer console.
@@ -76,11 +87,10 @@ class FakeAmazonPurchasingService : AmazonPurchasingService {
       }
     }
     val userDataResponse = UserDataResponseBuilder()
-      .apply {
-        this.userData = userData
-        requestId = RequestId()
-        this.requestStatus = requestStatus
-      }.build()
+      .setUserData(userData)
+      .setRequestId(RequestId())
+      .setRequestStatus(requestStatus)
+      .build()
     purchasingListener.onUserDataResponse(userDataResponse)
     return userDataResponse.requestId
   }
@@ -104,37 +114,107 @@ class FakeAmazonPurchasingService : AmazonPurchasingService {
       }
     }
     val productDataResponseBuilder = ProductDataResponseBuilder()
-      .apply {
-        this.productData = productData
-        requestId = RequestId()
-        this.requestStatus = requestStatus
-      }.build()
+      .setProductData(productData)
+      .setRequestId(RequestId())
+      .setRequestStatus(requestStatus)
+      .build()
     purchasingListener.onProductDataResponse(productDataResponseBuilder)
     return productDataResponseBuilder.requestId
   }
 
   override fun purchase(sku: String): RequestId {
-//    val purchaseResponseBuilder = PurchaseResponseBuilder()
-//      .apply {
-//        userData = ,
-//        requestId = RequestId()
-//        requestStatus = ,
-//        receipt = ReceiptBuilder(),
-//      }
-//      .setUserData()
-//      .setRequestId()
-//      .setRequestStatus()
-//      .setReceipt()
-    purchasingListener.onPurchaseResponse(null)
-    TODO("Not yet implemented")
+    when (status) {
+      FakeServiceStatus.Available -> {
+        if (!products.any { it.sku == sku }) {
+          val purchaseResponse = PurchaseResponseBuilder()
+            .apply {
+              requestId = RequestId()
+              requestStatus = PurchaseResponse.RequestStatus.INVALID_SKU
+            }.build()
+          purchasingListener.onPurchaseResponse(purchaseResponse)
+          return purchaseResponse.requestId
+        }
+        val targetProduct = products.first { it.sku == sku }
+        val receiptData = FakeAmazonReceiptData.create(
+          userId = amazonUser.userData.userId,
+          sku = targetProduct.sku,
+          productType = targetProduct.productType
+        )
+        fakeAmazonReceiptDb.add(receiptData)
+        val purchaseResponse = PurchaseResponseBuilder()
+          .setUserData(amazonUser.userData)
+          .setRequestId(RequestId())
+          .setRequestStatus(PurchaseResponse.RequestStatus.SUCCESSFUL)
+          .setReceipt(receiptData.receipt)
+          .build()
+        purchasingListener.onPurchaseResponse(purchaseResponse)
+        return purchaseResponse.requestId
+      }
+
+      FakeServiceStatus.Unavailable -> {
+        val purchaseResponse = PurchaseResponseBuilder()
+          .setRequestId(RequestId())
+          .setRequestStatus(PurchaseResponse.RequestStatus.FAILED)
+          .build()
+        purchasingListener.onPurchaseResponse(purchaseResponse)
+        return purchaseResponse.requestId
+      }
+    }
   }
 
   override fun notifyFulfillment(id: String, fulfillmentResult: FulfillmentResult) {
-    TODO("Not yet implemented")
+    val receiptData = fakeAmazonReceiptDb.getReceiptData(id) ?: return
+    // Delete only consume(in-app) product as it will no longer return receipts.
+    // https://developer.amazon.com/ja/docs/in-app-purchasing/iap-faqs.html#iap-api-questions
+    if (receiptData.receipt.productType == ProductType.CONSUMABLE && fulfillmentResult == FulfillmentResult.FULFILLED) {
+      fakeAmazonReceiptDb.remove(id)
+    } else {
+      fakeAmazonReceiptDb.update(
+        receiptData.copy(
+          fulfillmentResult = fulfillmentResult
+        )
+      )
+    }
   }
 
   override fun getPurchaseUpdates(requestAll: Boolean): RequestId {
-    purchasingListener.onPurchaseUpdatesResponse(null)
-    TODO("Not yet implemented")
+    when (status) {
+      FakeServiceStatus.Available -> {
+        val (userReceiptDataList, hasMore) = if (requestAll) {
+          val userReceiptDataList =
+            fakeAmazonReceiptDb.getReceiptDataList(amazonUser.userData.userId)
+          userReceiptDataList to false
+        } else {
+          val receiptDataList = fakeAmazonReceiptDb.getReceiptDataList(amazonUser.userData.userId)
+          val startIndex = (purchaseUpdatesPageNumber - 1) * purchaseUpdatesPageSize
+          val endIndex = minOf(startIndex + purchaseUpdatesPageSize, receiptDataList.size)
+          val hasMore = startIndex < receiptDataList.size
+          if (hasMore) {
+            purchaseUpdatesPageNumber++
+          }
+          val userReceiptDataList = receiptDataList.subList(startIndex, endIndex)
+          userReceiptDataList to hasMore
+        }
+
+        val purchaseUpdatesResponse = PurchaseUpdatesResponseBuilder()
+          .setRequestId(RequestId())
+          .setRequestStatus(PurchaseUpdatesResponse.RequestStatus.FAILED)
+          .setUserData(amazonUser.userData)
+          .setHasMore(hasMore)
+          .setReceipts(userReceiptDataList.map { it.receipt })
+          .build()
+        purchasingListener.onPurchaseUpdatesResponse(purchaseUpdatesResponse)
+        return purchaseUpdatesResponse.requestId
+      }
+
+      FakeServiceStatus.Unavailable -> {
+        val purchaseUpdatesResponse = PurchaseUpdatesResponseBuilder()
+          .setRequestId(RequestId())
+          .setRequestStatus(PurchaseUpdatesResponse.RequestStatus.FAILED)
+          .build()
+        purchasingListener.onPurchaseUpdatesResponse(purchaseUpdatesResponse)
+        return purchaseUpdatesResponse.requestId
+      }
+    }
   }
 }
